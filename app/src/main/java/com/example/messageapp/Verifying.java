ea03f84293e92f6688916e02f18dd6dbe3c087af
MUSKAAN <Muskaan.sharma@mechlintech.com>
2021-12-21 03:21:45 +0530

    Initial commit

--- app/src/main/java/com/example/messageapp/Verifying.java
+++ app/src/main/java/com/example/messageapp/Verifying.java
@@ -110,6 +110,7 @@ public class Verifying extends Fragment {
             }
         });
 
+
         callbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
